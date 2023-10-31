82064f9102f2ccb28e99caf5ac0e2ad6e2cf8e9a
sandwichcookice <139994905+sandwichcookice@users.noreply.github.com>
2023-11-01 00:13:55 +0800

    2023/11/1

---
+++ a004.cpp
@@ -0,0 +1,47 @@
+﻿// a004.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool leapyear=false;
+
+    int year;
+
+    while (cin >> year)
+    {
+        leapyear = false;
+        if (year % 4 == 0)
+        {
+            if (year % 100 != 0)
+                leapyear = true;
+            else
+            {
+                if (year % 400 == 0)
+                    leapyear = true;
+            }
+        }
+
+        if (leapyear == true)
+            cout << "閏年" << endl;
+        else
+            cout << "平年" << endl;
+
+    }
+
+    
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
---
+++ a006.cpp
@@ -0,0 +1,45 @@
+﻿// a006.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    //公式解 (-b +- b^2 - 4 * a * c) / 2 * a
+    // sqrt() <== 求平方根 *需include cmath
+
+    int a, b, c;
+
+    scanf_s("%d %d %d", &a, &b, &c);
+
+    float discriminant = b * b - 4 * a * c, x1, x2;
+
+    if (discriminant > 0)
+    {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Two different roots x1=" << x1 << " , x2=" << x2;
+    }
+    else if (discriminant == 0)
+    {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        cout << "Two same roots x=" << x1;
+    }
+    else
+        cout << "No real root";
+
+    return 0;
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
