96fbdea201f66ff6f1460e9e17d7df1460489ba9
sandwichcookice <139994905+sandwichcookice@users.noreply.github.com>
2024-01-10 20:23:51 +0800

    Add files via upload

---
+++ a058.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int time = 0;
+
+    int x = 0, y = 0, z = 0; //x=3k, y=3k+1, z=3k+2
+
+    cin >> time;
+
+    for (int i = 0; i < time; i++)
+    {
+        int mub = 0;
+        cin >> mub;
+        mub = mub % 3;
+        switch (mub)
+        {
+        case 0:
+            x++;
+            break;
+        case 1:
+            y++;
+            break;
+        case 2:
+            z++;
+            break;
+        default:
+            break;
+        }
+    }
+
+    printf("%d %d %d", x, y, z);
+
+    return 0;
+}
